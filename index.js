c6d9a691cc25d77401c0119c876171fad851eff7
Ayana Bando <ayanaflax@yahoo.com>
2019-01-23 14:27:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function showRepositories(event, data) {
   const repos = JSON.parse(this.responseText);
   const src = document.getElementById('repository-template').innerHTML;
     const template = Handlebars.compile(src);
-  const repoList = 
+  const repoList = template(repos);
   document.getElementById('repositories').innerHTML = repoList;
 }
 
